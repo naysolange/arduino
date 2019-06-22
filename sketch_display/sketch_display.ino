1ef18c85a3c841af0a72686d16ebd5b9dce7c16d
Nayla Portas <naylaportas@gmail.com>
2019-06-22 18:48:22 -0300

    Adapta el código para una pantalla de 16x1

--- sketch_display/sketch_display.ino
+++ sketch_display/sketch_display.ino
@@ -1,30 +1,27 @@
+#include <LiquidCrystal_I2C.h>
 
-// Prueba de Modulo I2C para LCD 2x16 
-#include <LiquidCrystal_I2C.h> 
-
-
-LiquidCrystal_I2C lcd(0x27,16,2);
-
+LiquidCrystal_I2C lcd(0x27,8,2);
 
 void setup() {
   lcd.init();
   lcd.backlight();
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print("Hola mundo..."); 
+  lcd.print("Hola mun"); // Mensaje a despegar
+  lcd.setCursor(0,1);
+  lcd.print("do!!"); // Mensaje a despegar
+  
   delay(3000);
 }
 
-void loop() {
-   
-  for(int i=0; i<12; i++){
+void loop() { 
+  /*
+  for(int c=0;c<15;c++){
     lcd.scrollDisplayLeft();
     delay(400);
   }
-  
-  for(int i=0; i<12; i++){
-    lcd.scrollDisplayRight();
-    delay(400); 
-  }
-  
+  for(int c=0; c<15;c++){
+  lcd.scrollDisplayRight();
+  delay(400); 
+  }*/
 }
