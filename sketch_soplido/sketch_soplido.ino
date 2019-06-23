c0c80fdda2aba20428f301647854d4f77394edfb
Nayla Portas <naylaportas@gmail.com>
2019-06-22 21:04:34 -0300

    Crea mini poesia con un soplido

--- sketch_soplido/sketch_soplido.ino
+++ sketch_soplido/sketch_soplido.ino
@@ -1,15 +1,22 @@
 #include <LiquidCrystal_I2C.h>
-
+#include <stdio.h>
 #define PIN_LED A0                // High side of the resistor
 #define PIN_MEDICION A6           // Low side of resistor and anode of LED
 #define DIFERENCIA_MINIMA 19.00           
 #define CANTIDAD_SENSADA 10
+#define CANTIDAD_PALABRAS 25
 
+// Variables globales
 LiquidCrystal_I2C lcd(0x27,8,2);
 long int dataSensada[CANTIDAD_SENSADA];
 long int dataSensada2[CANTIDAD_SENSADA];
 int j = 0;
 int k = 0;
+String palabras[CANTIDAD_PALABRAS] = {"fatigada","la noche","estoy mirando","en alto","tal dulzura","no preguntes","noche",
+"ojos","luna blanca","puedo escapar","cielo profundo","afuera llueve","por la vida","corazon","tus brazos", 
+"sin hacer ruido","salvame","era silencio","maquina","de un suspiro","la mirada","no entender","mueres de sed",
+"tus pupilas","perfume tenue"};
+
 
 void setup() {
   // Inicializa display
@@ -45,7 +52,7 @@ void loop() {
   if(j == CANTIDAD_SENSADA && k == CANTIDAD_SENSADA) {
     float promedio1 = promedioSuma(dataSensada);
     float promedio2 = promedioSuma(dataSensada2);
-    Serial.println(promedio2 - promedio1);
+    //Serial.println(promedio2 - promedio1);
 
     // Si la diferencia entre el promedio de los segundos valores leídos es mayor a 
     // un X valor entonces quiere decir que hubo una variación (alguien hizo un soplido)
@@ -54,8 +61,8 @@ void loop() {
         digitalWrite(PIN_LED, LOW);
         mostrarPalabra();            
         digitalWrite(PIN_LED, HIGH);
-        mostrarPalabra2();  
-        mostrarPalabra3(); 
+        mostrarPalabra();  
+        mostrarPalabra(); 
         lcd.clear();
         lcd.noBacklight(); 
     }
@@ -66,28 +73,29 @@ void loop() {
 }
 
 void mostrarPalabra() {
+
+  String palabra;
+  String palabraRecortada1;
+  String palabraRecortada2;
+  int posicion = random(CANTIDAD_PALABRAS);
+  Serial.println(posicion);
+  palabra = palabras[posicion];
+  int longitudPalabra = palabra.length();
+  int maxCaracteresPorRenglon = 8;
+  
+  if(longitudPalabra>maxCaracteresPorRenglon) {   
+    palabraRecortada1 = palabra.substring(0, 8);
+    palabraRecortada2 = palabra.substring(8, longitudPalabra);
+  } else {
+    palabraRecortada1 = palabra;
+  }
+ 
   lcd.clear();
-  lcd.setCursor(0,0);
   lcd.backlight();
-  lcd.print("Naturale"); // Mensaje a despegar
+  lcd.setCursor(0,0);
+  lcd.print(palabraRecortada1); 
   lcd.setCursor(0,1);
-  lcd.print("za"); // Mensaje a despegar
-  delay(3000);
-}
-
-void mostrarPalabra2() {
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.backlight();
-  lcd.print("Armadura"); // Mensaje a despegar
-  delay(3000);
-}
-
-void mostrarPalabra3() {
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.backlight();
-  lcd.print("Amor"); // Mensaje a despegar
+  lcd.print(palabraRecortada2); 
   delay(3000);
 }
 
