a1f1ebbcfebd327090e1ced887bb0b348b86b13b
Nayla Portas <naylaportas@gmail.com>
2020-02-08 20:55:46 -0300

    Scrolear una palabra

--- sketch_test_texto/sketch_test_texto.ino
+++ sketch_test_texto/sketch_test_texto.ino
@@ -15,42 +15,135 @@ int y = matrix.height();
 int x = matrix.width();
 int pass = 0;
 int line_pass = 0;
+int offset = 6;
+
+uint32_t colorTexto = matrix.Color(0, 90, 20);
 
 void setup() {
+  Serial.begin(9600);
   matrix.begin();
-  matrix.setTextWrap(false);
-  matrix.setBrightness(50);
-  matrix.setTextColor(matrix.Color(255,0,200));
 }
-boolean mostrarTexto = true;
-String texto = "";
 
 void loop() {
 
-  if(mostrarTexto){
-   texto = "prueba";
-   dibujar(texto);
-   mostrarTexto = false;
+  matrix.fillScreen(0);
+  matrix.setCursor(0, 0);
+ // matrix.setTextColor(matrix.Color(90,0,90));
+  //matrix.print("l");
+ //matrix.show();
+  
+  mostrarPoesia();
+}
+
+void mostrarPoesia() {
+
+  matrix.fillScreen(0);
+  matrix.setCursor(0, 0);
+  scroll("hola");
+   
+}
+
+void dibujarA(int offset) {
+    prenderPixel(1,2, offset, colorTexto);
+    prenderPixel(2,2, offset, colorTexto);
+    prenderPixel(3,3, offset, colorTexto);
+    prenderPixel(1,4, offset, colorTexto);
+    prenderPixel(2,4, offset, colorTexto);
+    prenderPixel(3,4, offset, colorTexto);
+    prenderPixel(0,5, offset, colorTexto);
+    prenderPixel(3,5, offset, colorTexto);
+    prenderPixel(1,6, offset, colorTexto);
+    prenderPixel(2,6, offset, colorTexto);
+    prenderPixel(3,6, offset, colorTexto);
+    prenderPixel(4,6, offset, colorTexto);
+}
+
+void dibujarH(int offset) {
+    prenderPixel(0, 0, offset, colorTexto);
+    prenderPixel(0, 1, offset, colorTexto);
+    prenderPixel(0, 2, offset, colorTexto);
+    prenderPixel(2, 2, offset, colorTexto);
+    prenderPixel(3, 2, offset, colorTexto);
+    prenderPixel(0, 3, offset, colorTexto);
+    prenderPixel(1, 3, offset, colorTexto);
+    prenderPixel(4, 3, offset, colorTexto);
+    prenderPixel(0, 4, offset, colorTexto);
+    prenderPixel(4, 4, offset, colorTexto);
+    prenderPixel(0, 5, offset, colorTexto);
+    prenderPixel(4, 5, offset, colorTexto);
+    prenderPixel(0, 6, offset, colorTexto);
+    prenderPixel(4, 6, offset, colorTexto);
+}
+
+
+void dibujarO(int offset) {
+    prenderPixel(1,2, offset, colorTexto);
+    prenderPixel(2,2, offset, colorTexto);
+    prenderPixel(3,2, offset, colorTexto); 
+    prenderPixel(0,3, offset, colorTexto);
+    prenderPixel(4,3, offset, colorTexto);
+    prenderPixel(0,4, offset, colorTexto);
+    prenderPixel(4,4, offset, colorTexto);
+    prenderPixel(0,5, offset, colorTexto);
+    prenderPixel(4,5, offset, colorTexto);
+    prenderPixel(1,6, offset, colorTexto);
+    prenderPixel(2,6, offset, colorTexto);
+    prenderPixel(3,6, offset, colorTexto);
+}
+
+
+void dibujarL(int offset) {
+    prenderPixel(1,0, offset, colorTexto);
+    prenderPixel(2,0, offset, colorTexto);
+    prenderPixel(2,1, offset, colorTexto);
+    prenderPixel(2,2, offset, colorTexto);
+    prenderPixel(2,3, offset, colorTexto);
+    prenderPixel(2,4, offset, colorTexto);
+    prenderPixel(2,5, offset, colorTexto);
+    prenderPixel(2,6, offset, colorTexto);
+    prenderPixel(1,6, offset, colorTexto);
+    prenderPixel(3,6, offset, colorTexto);
+}
+
+void prenderPixel(int x, int y, int offset, uint32_t colorTexto) {
+
+  if(x+offset <= 59 && x+offset >= 0) {
+    matrix.setPixelColor(obtenerPixel(x+offset,y), colorTexto);
+  }
+  
+}
+
+int obtenerPixel(int x, int y) {
+
+  if(y % 2 > 0) {
+    return (60*y+59)-x;
+  }
+  
+  return y*60 + x;
+}
+
+void scroll(char* message) {
+  
+  int nuevoOffset = 60;
+  int i = 0;
+
+  while(true) {
+    Serial.println(nuevoOffset);
+    dibujarH(nuevoOffset);
+    dibujarO(nuevoOffset+offset);
+    dibujarL(nuevoOffset+offset*2);
+    dibujarA(nuevoOffset+offset*3);
+    matrix.show();
+    delay(30);
+    matrix.clear();
+    
+    if(nuevoOffset+offset*3 < 0) {
+      Serial.println("entroooo");
+      matrix.show();
+      break;
+    }
+    
+    nuevoOffset--;
   }
  
-}
-
-void dibujar(String texto) {
-  while(true) {
-    matrix.clear(); 
-    matrix.fillScreen(0);
-    matrix.setCursor(x, 0);
-    scroll(texto.c_str(), 40);
-  }
-}
-
-void scroll(char* message, int delays) {
-  int maxDisplacement = strlen(message) * pixelPerChar + matrix.width();
-  if(++line_pass > matrix.width()) line_pass = 0;
-  matrix.print(String(message));
-  if(--x < -maxDisplacement) {
-    x = matrix.width();
-  }
-  matrix.show();
-  delay(delays);
 }
